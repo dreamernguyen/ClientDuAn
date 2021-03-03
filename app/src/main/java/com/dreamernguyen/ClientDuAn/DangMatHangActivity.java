701881fa734f356b16912822b01740d202028aa0
tri <triqmpd03233@fpt.edu.vn>
2021-03-03 16:22:41 +0700

    day

--- app/src/main/java/com/dreamernguyen/ClientDuAn/DangMatHangActivity.java
+++ app/src/main/java/com/dreamernguyen/ClientDuAn/DangMatHangActivity.java
@@ -23,6 +23,9 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.cloudinary.android.MediaManager;
+import com.cloudinary.android.callback.ErrorInfo;
+import com.cloudinary.android.callback.UploadCallback;
 import com.dreamernguyen.ClientDuAn.Adapter.AnhAdapter;
 import com.dreamernguyen.ClientDuAn.Models.BaiViet;
 import com.dreamernguyen.ClientDuAn.Models.DuLieuTraVe;
@@ -30,6 +33,8 @@ import com.dreamernguyen.ClientDuAn.Models.MatHang;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 
 import gun0912.tedbottompicker.TedBottomPicker;
 import gun0912.tedbottompicker.TedBottomSheetDialogFragment;
@@ -46,6 +51,7 @@ public class DangMatHangActivity extends AppCompatActivity {
     RecyclerView rvAnh;
     ImageView imgChonAnh;
     String idMatHang;
+    List<Uri> listPath = new ArrayList<>();
 
     TextView tvTieuDe, tvDangtin;
 
@@ -107,7 +113,7 @@ public class DangMatHangActivity extends AppCompatActivity {
             tvDangtin.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    dangMatHang();
+                    dangMatHang(null);
                 }
             });
             Toast.makeText(this, "Tạo bài mới", Toast.LENGTH_SHORT).show();
@@ -141,6 +147,96 @@ public class DangMatHangActivity extends AppCompatActivity {
         });
     }
 
+    private void upAnhLenServer() {
+
+        String regex = "(http://|https://|www\\.)([^ '\"]*)";
+        List<String> anhCu = new ArrayList<>();
+        List<Uri> anhMoi = new ArrayList<>();
+//        Pattern pattern = Pattern.compile(regex);
+//        for (int i = 0; i < listAnhCu.size(); i ++){
+//            if(!pattern.matcher(listAnhCu.get(i)).matches()){
+//                listPath.add(Uri.parse(listAnhCu.get(i)));
+//            }
+//        }
+        listPath.clear();
+        for (String uri : listAnh){
+            if(!Pattern.compile(regex).matcher(uri).matches()){
+                anhMoi.add(Uri.parse(uri));
+            }else {
+                anhCu.add(uri);
+            }
+
+        }
+        Log.d("list", "upAnhLenServer: "+listAnh);
+        Log.d("anhCu", "upAnhLenServer: "+anhCu);
+        Log.d("anhMoi", "upAnhLenServer: "+anhMoi);
+
+        if(listAnh.size() > 0){
+            Log.d("ccccc", "upAnhLenServer:  Có ảnh");
+
+            Log.d("oooo", "onClick: "+listPath);
+            List<String> listURL = new ArrayList<>();
+            for(int i = 0 ;i < anhMoi.size();i++){
+                Log.d("ooo", "onImagesSelected: "+anhMoi.get(i));
+                MediaManager.get().upload(anhMoi.get(i))
+                        .unsigned("gybczcnv").callback(new UploadCallback() {
+                    @Override
+                    public void onStart(String requestId) {
+
+                    }
+
+                    @Override
+                    public void onProgress(String requestId, long bytes, long totalBytes) {
+
+                    }
+
+                    @Override
+                    public void onSuccess(String requestId, Map resultData) {
+                        Log.d("trave", "onSuccess: "+resultData.get("url"));
+
+                        if(idMatHang == null){
+                            listURL.add(resultData.get("url").toString());
+                            Log.d("--", "onSuccess: "+listURL);
+                            if(listURL.size() == anhMoi.size()){
+                                Log.d("cuoicungroiuii", "loadAnh: "+listURL);
+
+
+                            }
+                            dangMatHang(listURL);
+                            Log.d("Hihi", "onSuccess: Đăng bài viết");
+                        }
+                        else {
+                            listURL.add(resultData.get("url").toString());
+                            Log.d("--", "onSuccess: "+anhCu);
+                            if(listURL.size() == anhMoi.size()){
+                                Log.d("cuoicungroiuii", "loadAnh: "+listURL);
+                                listURL.addAll(anhCu);
+
+                            }
+                            capNhatMatHang(listURL);
+                            Log.d("Hihi", "onSuccess: Cập nhật bài viết");
+                        }
+                    }
+
+                    @Override
+                    public void onError(String requestId, ErrorInfo error) {
+
+                    }
+
+                    @Override
+                    public void onReschedule(String requestId, ErrorInfo error) {
+
+                    }
+                }).dispatch();
+
+            }
+        }
+        else {
+            Log.d("ddd", "upAnhLenServer: Không có ảnh");
+            dangMatHang(null);
+        }
+
+    }
 
     public void KhaiBao() {
         edTieuDe = findViewById(R.id.edTieude);
@@ -154,7 +250,7 @@ public class DangMatHangActivity extends AppCompatActivity {
         finish();
     }
 
-    public void dangMatHang() {
+    public void dangMatHang( List<String> listUrl) {
 
         Integer giaBan = Integer.parseInt(edGiaBan.getText().toString());
 
@@ -164,7 +260,7 @@ public class DangMatHangActivity extends AppCompatActivity {
 
         List<String> linkAnh = new ArrayList<>();
         linkAnh.add("https://techland.com.vn/wp-content/uploads/2019/09/dien-thoai-iphone-11-pro-max-3a.jpg");
-        MatHang matHang = new MatHang(tieuDe, noiDung, spnHangMuc.getSelectedItem().toString(), giaBan, listAnh, diaChi);
+        MatHang matHang = new MatHang(tieuDe, noiDung, spnHangMuc.getSelectedItem().toString(), giaBan, listUrl, diaChi);
 
         Call<DuLieuTraVe> call = ApiService.apiService.dangMatHang("6006875981484b2c7c2176c5", matHang);
         call.enqueue(new Callback<DuLieuTraVe>() {
